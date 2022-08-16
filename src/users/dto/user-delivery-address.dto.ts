fb6f7fb751b13ee28f777127a005be334a4445b9
Fahmi dwi s <dwifhmi.developer@gmail.com>
2022-08-16 22:01:44 +0700

    - enhance user delivery address

--- src/users/dto/user-delivery-address.dto.ts
+++ src/users/dto/user-delivery-address.dto.ts
@@ -8,7 +8,45 @@ export class UserDeliveryAddressDto {
 
   @IsDefined()
   @IsNotEmpty()
-  address_name: string;
+  name: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  phone_number: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  street_name: string;
+
+  @IsOptional()
+  detail_other: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  province: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  district: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  village: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  rt: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  rw: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  postal_code: string;
+
+  @IsOptional()
+  address_type: number;
 
   @IsOptional()
   selected: boolean;
--- src/users/entities/user-delivery-address.entity.ts
+++ src/users/entities/user-delivery-address.entity.ts
@@ -13,9 +13,39 @@ export class UserDeliveryAddress {
   uuid: string;
 
   @Column({ length: 50 })
-  address_name: string;
+  name: string;
+
+  @Column({ length: 50 })
+  phone_number: string;
 
   @Column()
+  street_name: string;
+
+  @Column({ nullable: true })
+  detail_other: string;
+
+  @Column({ length: 50 })
+  province: string;
+
+  @Column({ length: 50 })
+  district: string;
+
+  @Column({ length: 50 })
+  village: string;
+
+  @Column({ length: 10 })
+  rt: string;
+
+  @Column({ length: 10 })
+  rw: string;
+
+  @Column({ length: 10 })
+  postal_code: string;
+
+  @Column({ nullable: true })
+  address_type: number; // 0 for office, 1 for home
+
+  @Column({ default: 0 })
   selected: boolean;
 
   @ManyToOne(() => User, (user) => user.uuid)
